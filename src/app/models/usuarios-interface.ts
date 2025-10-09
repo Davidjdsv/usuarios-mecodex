b9e0dda27be3fb9208789ecebda2791fa0d8d1bb
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-10-09 11:49:02 -0500

    feat: -Configurando el env para obtener los datos de la api -Modificando la interface para obtener exactamente los datos que manda la API -Obteniendo por método http.get y mapeando los datos filtrados para mandarlos a la vista

--- src/app/models/usuarios-interface.ts
+++ src/app/models/usuarios-interface.ts
@@ -7,21 +7,28 @@ export interface UsuariosInterface {
     telefono: string;
     direccion: string;
     id_tipo_documento: number; // ID de la tabla tipo_documento... Debería de ser un objeto del tipo tipo_documento?
-    documento: number;
+    documento: string;
     fecha_expedicion: string;
     correo: string;
+    id_pais: number;
     pais_nombre: string; // ID de la tabla paises... Debería de ser un objeto del tipo país?
-    plan_mecodex: string;
-    observacion_cliente: string;
-    observacion_comercial: string;
-    observacion_soporte: string;
-    observacion_cuenta: string;
+    PLAN_MECODEX: string;
+    observacion_cliente: string | null;
+    observacion_comercial: string | null;
+    observacion_soporte: string | null;
+    observacion_cuenta: string | null;
     fecha_creacion: string;
-    id_usuario_sensei: number;
-    fecha_modificacion: string;
+    id_usuario_sensei?: number;
+    fecha_modificacion?: string;
+    id_licencia?: number;
+    pais_iso?: string;
+    tipo_documento_nombre?: string;
+    tipo_documento_abreviatura?: string;
+    usuario_sensei_nombre?: string;
 }
 
 export interface UsuariosResponseInterface {
-    success: boolean;
+    succes: boolean;
     data: UsuariosInterface[];
+    total: number;
 }
