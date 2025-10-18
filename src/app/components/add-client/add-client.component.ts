c47f15fcf9ffa908e1e94d8fcdc0b915a62e7bce
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-10-18 11:57:02 -0500

    feat: -Agregando el html del componente para agregar un nuevo cliente -Agregando estilos sccss para component separando un poco entre inputs

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,14 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonSelect,
+  IonSelectOption,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { UsuariosInterface } from 'src/app/models/usuarios-interface';
 
 @Component({
   selector: 'app-add-client',
   templateUrl: './add-client.component.html',
   styleUrls: ['./add-client.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonSelect,
+    IonSelectOption,
+    IonButton,
+  ],
 })
-export class AddClientComponent  implements OnInit {
+export class AddClientComponent implements OnInit {
+  // Modelo para enlazar con el formulario usando ngModel
+  nuevoUsuario: Partial<UsuariosInterface> = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
