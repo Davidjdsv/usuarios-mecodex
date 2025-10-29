f138feaa5dbc170a985d3366217676030d5c8b46
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-10-29 15:27:19 -0500

    docs: -Añadiendo cambios en el config.ts para desplegar

--- Mecodex docs/docusaurus.config.ts
+++ Mecodex docs/docusaurus.config.ts
@@ -5,9 +5,10 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Docs Mecodex',
+  tagline: 'Documentación del dashboard de Mecodex e información para el encargado del área de soporte',
   favicon: 'img/favicon.ico',
+  deploymentBranch: 'gh-pages',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,17 +16,20 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // url: 'https://your-docusaurus-site.example.com',
+  url: 'https://davidjdsv.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/usuarios-mecodex/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Davidjdsv', // Usually your GitHub org/user name.
+  projectName: 'usuarios-mecodex', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -34,6 +38,7 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en', 'es'],
   },
+
 
   presets: [
     [
@@ -75,9 +80,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Docs Mecodex',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Docs Mecodex Logo',
         src: 'img/logo.svg',
       },
       items: [
