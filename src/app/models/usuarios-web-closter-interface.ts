ec544dcab48acac863fe1592ae2d249d7773cd91
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-11-04 12:04:58 -0500

    cambios

---
+++ src/app/models/usuarios-web-closter-interface.ts
@@ -0,0 +1,10 @@
+export interface UsuariosWebClosterInterface {
+    id_usuario_wc: number;
+    nombre_completo: string;
+    documento: string;
+    contacto: string;
+    correo: string;
+    id_rol_usuario: number;
+    nombre_usuario: string;
+    contrasena: string;
+}
