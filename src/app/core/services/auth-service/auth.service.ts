1ad7f8980a2f4ad14d075ab29d7cc3373144cf95
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-11-10 10:21:01 -0500

    feat: -Implementado servicio para obtener el token en auth-service.ts

--- src/app/core/services/auth-service/auth.service.ts
+++ src/app/core/services/auth-service/auth.service.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { UsuariosWebClosterInterface } from 'src/app/models/usuarios-web-closter-interface';
+import { LoginResponseInterface, UsuariosWebClosterInterface } from 'src/app/models/usuarios-web-closter-interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,30 +12,48 @@ export class AuthService {
   private isLoggedIn = signal(false); // Bandera en primer estado falso para verificar si el usuario está autenticado
 
   private apiLoginURL = signal(environment.api_usuarios_web_closter); // Será tanto para las tareas del CRUD como para obtener token
-  // private apiLoginUWCURL = signal(environment.api_login_uwc);
+  private tokeKey: string = "authToken";
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  loginUsuarioService(nombre_usuario: string, contrasena: string): Observable<UsuariosWebClosterInterface>{
-    const url = new URL(`${this.apiLoginURL()}?login`);
-    url.searchParams.append('nombre_usuario', nombre_usuario);
-    url.searchParams.append('contrasena', contrasena);
-    return this.http.get<UsuariosWebClosterInterface>(url.toString());
+  loginUsuarioService(nombre_usuario: string, contrasena: string): Observable<LoginResponseInterface>{
+    const url = (`${this.apiLoginURL()}?login`);
+    return this.http.post<LoginResponseInterface>(url.toString(), { 
+      usuario: nombre_usuario,
+      contrasena: contrasena }).pipe(
+      tap(res => {
+        if(res.token){
+          console.log(res.token)
+          this.setToken(res.token);
+          this.isLoggedIn.set(true);
+        }
+      })
+    )
   }
-  // /**
-  //  * Enviar credenciales al backend y recibir token
-  //  * @param usuario - Nombre de usuario o email
-  //  * @param clave - Contraseña
-  //  * @returns Observable con la respuesta del servidor
-  //  */
-  // login(usuario: string, clave: string): Observable<any> {
-  //   return this.http.post<any>(this.apiLoginURL(), { usuario, clave }).pipe(
-  //     tap(res =>{
-  //       if(res.token){
-  //         console.log(res.token)
-  //       }
-  //     })
-  //   )
-  // }
+
+  private getToken(): string | null{
+    return localStorage.getItem(this.tokeKey);
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokeKey, token);
+  }
+
+  isAutenthicate(): boolean {
+    const token = this.getToken();
+    if(!token){
+      return false   
+    }
+
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    const exp = payload.exp * 1000;
+    return Date.now() < exp;
+  }
+
+  logOut(): void {
+    localStorage.removeItem(this.tokeKey)
+    this.isLoggedIn.set(false);
+    this.router.navigate(['/login']);
+  }
 
 }
--- src/app/models/usuarios-web-closter-interface.ts
+++ src/app/models/usuarios-web-closter-interface.ts
@@ -1,3 +1,4 @@
+// Para múltiples usuarios
 export interface UsuariosWebClosterInterface {
     id_usuario_wc: number;
     nombre_completo: string;
@@ -9,8 +10,18 @@ export interface UsuariosWebClosterInterface {
     contrasena: string;
 }
 
+// Para LOGIN (un solo usuario)
+export interface LoginResponseInterface {
+    success: boolean;
+    message: string;
+    token: string;
+    data: UsuariosWebClosterInterface;
+}
+
 export interface UsuariosWebClosterResponseInterface{
     success: boolean;
+    message: string;
+    token: string;
     data: UsuariosWebClosterInterface[];
     total: number;
 }
