2d64f6ba37002cb48e84e1d301e5280efb1f435b
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-11-13 11:34:34 -0500

    feat: -Añadido el select para mostrar los roles del usuario a asignar -Añadido método que consulta al servicio -Método como servicio para consultar el back y la bd -Añadido exitosamente la agregación de usuarios de webcloster al sistema

--- src/app/components/webcloster/add-usuarios-wc/add-usuarios-wc.component.ts
+++ src/app/components/webcloster/add-usuarios-wc/add-usuarios-wc.component.ts
@@ -16,8 +16,9 @@ import {
 } from '@ionic/angular/standalone';
 import { UsuariosWebClosterService } from 'src/app/core/services/usuarios-webcloster.service';
 import { UsuariosWebClosterInterface } from 'src/app/models/usuarios-web-closter-interface';
+import { RolesInterface } from 'src/app/models/roles-interface';
+import { RolesUsuariosService } from 'src/app/core/services/roles-usuarios.service';
 import { PaisesInterface } from 'src/app/models/paises-interface';
-import { PaisServicioService } from 'src/app/core/services/pais-servicio.service';
 import { DocumentosInterface } from 'src/app/models/documentos-interface';
 import { DocumentosService } from 'src/app/core/services/documentos.service';
 
@@ -46,13 +47,16 @@ export class AddUsuariosWcComponent  implements OnInit {
   // para poder mostrarlas en los select de la vista según traiga la información el servicio
   paises = signal<PaisesInterface[]>([]);
   documentos = signal<DocumentosInterface[]>([]);
-
+  roles = signal<RolesInterface[]>([]);
+  
   // Porque ajá, es mucho complique el signal en llamado a [(ngModel)], mejor el uso normal
+  dataUsuarioWc1 = signal<UsuariosWebClosterInterface[]>([]);
   dataUsuarioWc = {
       id_usuario_wc: 0,
       id_tipo_documento: 0,
       nombre_completo: '',
       correo: '',
+      id_rol_usuario: 0,
       contrasena: '',
       documento: '',
       contacto: '',
@@ -62,8 +66,8 @@ export class AddUsuariosWcComponent  implements OnInit {
   // Inyección de dependencias
   constructor(private modalController: ModalController, 
     private usuariosWebClosterService: UsuariosWebClosterService, 
-    private paisServicioService: PaisServicioService, 
-    private documentosService: DocumentosService) { }
+    private documentosService: DocumentosService,
+  private rolesUsuariosService: RolesUsuariosService) { }
 
   ngOnInit() {}
 
@@ -71,6 +75,20 @@ export class AddUsuariosWcComponent  implements OnInit {
     this.documentosService.getDocuments().subscribe({
       next: (res: DocumentosInterface[]) => {
         this.documentos.set(res)
+      },
+      error: (err) => {
+        console.error("Error al listar documentos: ", err.message)
+      }
+    })
+  }
+
+  listarRoles(){
+    this.rolesUsuariosService.getRoles().subscribe({
+      next: (res: RolesInterface[]) => {
+        this.roles.set(res)
+      },
+      error: (err) => {
+        console.error("Error al listar roles: ", err.message)
       }
     })
   }
