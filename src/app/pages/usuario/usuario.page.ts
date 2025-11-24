6e77f95759d68ee5de83a43cf5b19bd32d1ecbf4
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-11-24 17:45:30 -0500

    refactor: -Refactorizando el código en usuario.page.ts para quedar mejor optimizado con signals

--- src/app/pages/usuario/usuario.page.ts
+++ src/app/pages/usuario/usuario.page.ts
@@ -90,119 +90,127 @@ export class UsuarioPage implements OnInit {
 
   licencias = signal<LicenciaInterface[]>([]);
 
-  usuarios: UsuariosInterface[] = [];
-  cuentaUsuario: CuentaInterface[] = [];
+  // ? Obtener TODOS los usuarios para luego ser filtrados en el método
+  usuarios = signal<UsuariosInterface[]>([]);
+
+  cuentaUsuario = signal<CuentaInterface[]>([]);
 
   usuarioActual: UsuariosInterface | undefined = undefined;
 
+  // Variables para almacenar los datos del cliente y de la cuenta que son de tipo interface
   // * Datos del cliente
   id = signal<number | null>(null);
-  nombre = signal<string | null>(null);
-  segundo_nombre = signal<string | null>(null);
-  apellido = signal<string | null>(null);
-  segundo_apellido = signal<string | null>(null);
-  telefono = signal<string | null>(null);
-  direccion = signal<string | null>(null);
-  id_tipo_documento = signal<number | null>(null);
-  abreviatura = signal<string | null>(null);
-  estado = signal<string | null>(null);
-  modo_conexion = signal<string | null>(null);
-  version_app = signal<string | null>(null);
-  fecha_calificacion = signal<string | null>(null);
-  calificacion = signal<number | null>(null);
-  mensaje_calificacion = signal<string | null>(null);
-  documento = signal<string | null>(null);
-  fecha_expedicion = signal<string | null>(null);
-  correo = signal<string | null>(null);
-  id_pais = signal<number | null>(null);
-  pais_nombre = signal<string | null>(null);
-  PLAN_MECODEX = signal<string | null>(null);
-  observacion_cliente = signal<string | null>(null);
-  observacion_comercial = signal<string | null>(null);
-  observacion_soporte = signal<string | null>(null);
-  observacion_cuenta = signal<string | null>(null);
-  fecha_creacion = signal<string | null>(null);
-  id_usuario_sensei = signal<number | null>(null);
-  fecha_modificacion = signal<string | null>(null);
-
+  useUsuario = signal<UsuariosInterface | undefined>(undefined);
   // * Datos de la cuenta
-  cuenta_correo = signal<string | null>(null);
-  cuenta_estado = signal<string | null>(null);
-  cuenta_version_app = signal<string | null>(null);
-  cuenta_modo_conexion = signal<string | null>(null);
-  cuenta_fecha_calificacion = signal<string | null>(null);
-  cuenta_mensaje_calificacion = signal<string | null>(null);
-  cuenta_calificacion = signal<number | null>(null);
-  cuenta_plan_mecodex = signal<string | null>(null);
-  cuenta_idapp_sta = signal<string | null>(null);
-  cuenta_idapp_din = signal<string | null>(null);
-  cuenta_fecha_creacion_cuenta = signal<string | null>(null);
-  password = signal<string | null>(null);
-  fecha_actualizacion_modo_conexion = signal<string | null>(null);
-  sincronizacion_total = signal<string | null>(null);
+  useCuenta = signal<CuentaInterface | undefined>(undefined);
+
+  // nombre = signal<string | null>(null);
+  // segundo_nombre = signal<string | null>(null);
+  // apellido = signal<string | null>(null);
+  // segundo_apellido = signal<string | null>(null);
+  // telefono = signal<string | null>(null);
+  // direccion = signal<string | null>(null);
+  // id_tipo_documento = signal<number | null>(null);
+  // abreviatura = signal<string | null>(null);
+  // estado = signal<string | null>(null);
+  // modo_conexion = signal<string | null>(null);
+  // version_app = signal<string | null>(null);
+  // fecha_calificacion = signal<string | null>(null);
+  // calificacion = signal<number | null>(null);
+  // mensaje_calificacion = signal<string | null>(null);
+  // documento = signal<string | null>(null);
+  // fecha_expedicion = signal<string | null>(null);
+  // correo = signal<string | null>(null);
+  // id_pais = signal<number | null>(null);
+  // pais_nombre = signal<string | null>(null);
+  // PLAN_MECODEX = signal<string | null>(null);
+  // observacion_cliente = signal<string | null>(null);
+  // observacion_comercial = signal<string | null>(null);
+  // observacion_soporte = signal<string | null>(null);
+  // observacion_cuenta = signal<string | null>(null);
+  // fecha_creacion = signal<string | null>(null);
+  // id_usuario_sensei = signal<number | null>(null);
+  // fecha_modificacion = signal<string | null>(null);
+
+  // cuenta_correo = signal<string | null>(null);
+  // cuenta_estado = signal<string | null>(null);
+  // cuenta_version_app = signal<string | null>(null);
+  // cuenta_modo_conexion = signal<string | null>(null);
+  // cuenta_fecha_calificacion = signal<string | null>(null);
+  // cuenta_mensaje_calificacion = signal<string | null>(null);
+  // cuenta_calificacion = signal<number | null>(null);
+  // cuenta_plan_mecodex = signal<string | null>(null);
+  // cuenta_idapp_sta = signal<string | null>(null);
+  // cuenta_idapp_din = signal<string | null>(null);
+  // cuenta_fecha_creacion_cuenta = signal<string | null>(null);
+  // password = signal<string | null>(null);
+  // fecha_actualizacion_modo_conexion = signal<string | null>(null);
+  // sincronizacion_total = signal<string | null>(null);
 
 
   ngOnInit() {
     // A esta actual ruta, tome el id
     this.id.set(Number(this.activatedRoute.snapshot.paramMap.get('id')));
     this.getUser();
+    this.getCuentaUsuario();
   }
 
   getUser() {
     this.usuariosService.getUsuarios().subscribe({
       next: (res) => {
-        this.usuarios = res; // guarda todos los usuarios del servicio en este array
-        const usuarioEncontrado = this.usuarios.find(
+        this.usuarios.set(res); // guarda todos los usuarios del servicio en este array
+        const usuarioEncontrado = this.usuarios().find(
           (usuario) => usuario.id === Number(this.id())
         );
-        this.usuarioActual = usuarioEncontrado;
-        if (usuarioEncontrado) {
-          this.nombre.set(usuarioEncontrado.nombre);
-          this.segundo_nombre.set(usuarioEncontrado.segundo_nombre);
-          this.apellido.set(usuarioEncontrado.apellido);
-          this.segundo_apellido.set(usuarioEncontrado.segundo_apellido);
-          this.telefono.set(usuarioEncontrado.telefono);
-          this.direccion.set(usuarioEncontrado.direccion);
-          this.id_tipo_documento.set(usuarioEncontrado.id_tipo_documento);
-          this.abreviatura.set(usuarioEncontrado.abreviatura || null);
-          this.estado.set(usuarioEncontrado.estado || null);
-          this.modo_conexion.set(usuarioEncontrado.modo_conexion || null);
-          this.version_app.set(usuarioEncontrado.version_app || null);
-          this.fecha_calificacion.set(
-            usuarioEncontrado.fecha_calificacion || null
-          );
-          this.calificacion.set(usuarioEncontrado.calificacion || null);
-          this.mensaje_calificacion.set(
-            usuarioEncontrado.mensaje_calificacion || null
-          );
-          this.documento.set(usuarioEncontrado.documento || null);
-          this.fecha_expedicion.set(usuarioEncontrado.fecha_expedicion || null);
-          this.correo.set(usuarioEncontrado.correo || null);
-          this.id_pais.set(usuarioEncontrado.id_pais || null);
-          this.pais_nombre.set(usuarioEncontrado.pais_nombre || null);
-          this.PLAN_MECODEX.set(usuarioEncontrado.PLAN_MECODEX || null);
-          this.observacion_cliente.set(
-            usuarioEncontrado.observacion_cliente || null
-          );
-          this.observacion_comercial.set(
-            usuarioEncontrado.observacion_comercial || null
-          );
-          this.observacion_soporte.set(
-            usuarioEncontrado.observacion_soporte || null
-          );
-          this.observacion_cuenta.set(
-            usuarioEncontrado.observacion_cuenta || null
-          );
-          this.fecha_creacion.set(usuarioEncontrado.fecha_creacion || null);
-          this.id_usuario_sensei.set(
-            usuarioEncontrado.id_usuario_sensei || null
-          );
-          this.fecha_modificacion.set(
-            usuarioEncontrado.fecha_modificacion || null
-          );
-          console.log(this.usuarioActual);
-          this.getCuentaUsuario();
-        }
+        // this.usuarioActual = usuarioEncontrado;
+        this.useUsuario.set(usuarioEncontrado);
+        // if (usuarioEncontrado) {
+        //   this.nombre.set(usuarioEncontrado.nombre);
+        //   this.segundo_nombre.set(usuarioEncontrado.segundo_nombre);
+        //   this.apellido.set(usuarioEncontrado.apellido);
+        //   this.segundo_apellido.set(usuarioEncontrado.segundo_apellido);
+        //   this.telefono.set(usuarioEncontrado.telefono);
+        //   this.direccion.set(usuarioEncontrado.direccion);
+        //   this.id_tipo_documento.set(usuarioEncontrado.id_tipo_documento);
+        //   this.abreviatura.set(usuarioEncontrado.abreviatura || null);
+        //   this.estado.set(usuarioEncontrado.estado || null);
+        //   this.modo_conexion.set(usuarioEncontrado.modo_conexion || null);
+        //   this.version_app.set(usuarioEncontrado.version_app || null);
+        //   this.fecha_calificacion.set(
+        //     usuarioEncontrado.fecha_calificacion || null
+        //   );
+        //   this.calificacion.set(usuarioEncontrado.calificacion || null);
+        //   this.mensaje_calificacion.set(
+        //     usuarioEncontrado.mensaje_calificacion || null
+        //   );
+        //   this.documento.set(usuarioEncontrado.documento || null);
+        //   this.fecha_expedicion.set(usuarioEncontrado.fecha_expedicion || null);
+        //   this.correo.set(usuarioEncontrado.correo || null);
+        //   this.id_pais.set(usuarioEncontrado.id_pais || null);
+        //   this.pais_nombre.set(usuarioEncontrado.pais_nombre || null);
+        //   this.PLAN_MECODEX.set(usuarioEncontrado.PLAN_MECODEX || null);
+        //   this.observacion_cliente.set(
+        //     usuarioEncontrado.observacion_cliente || null
+        //   );
+        //   this.observacion_comercial.set(
+        //     usuarioEncontrado.observacion_comercial || null
+        //   );
+        //   this.observacion_soporte.set(
+        //     usuarioEncontrado.observacion_soporte || null
+        //   );
+        //   this.observacion_cuenta.set(
+        //     usuarioEncontrado.observacion_cuenta || null
+        //   );
+        //   this.fecha_creacion.set(usuarioEncontrado.fecha_creacion || null);
+        //   this.id_usuario_sensei.set(
+        //     usuarioEncontrado.id_usuario_sensei || null
+        //   );
+        //   this.fecha_modificacion.set(
+        //     usuarioEncontrado.fecha_modificacion || null
+        //   );
+        //   console.log(this.usuarioActual);
+        //   this.getCuentaUsuario();
+        // }
       },
       error(err: any) {
         console.error('Error al obtener el usuario:', err);
@@ -224,19 +232,20 @@ export class UsuarioPage implements OnInit {
   getCuentaUsuario(){
     this.cuentaService.getCuenta().subscribe({
       next: (res) => {
-        this.cuentaUsuario = res
+        this.cuentaUsuario.set(res)
         const cuenta = res.find(c => c.id_cliente === Number(this.id()));
         if (cuenta) {
-          this.cuenta_correo.set(cuenta.correo || null);
-          this.cuenta_estado.set(cuenta.estado || null);
-          this.cuenta_version_app.set(cuenta.version_app || null);
-          this.cuenta_modo_conexion.set(cuenta.modo_conexion || null);
-          this.cuenta_fecha_calificacion.set(cuenta.fecha_calificacion || null);
-          this.cuenta_mensaje_calificacion.set(cuenta.mensaje_calificacion || null);
-          this.cuenta_calificacion.set(cuenta.calificacion || null);
-          this.cuenta_plan_mecodex.set(cuenta.plan_mecodex || null);
-          this.cuenta_idapp_sta.set(cuenta.idapp_sta || null);
-          this.cuenta_idapp_din.set(cuenta.idapp_din || null);
+          this.useCuenta.set(cuenta);
+          // this.cuenta_correo.set(cuenta.correo || null);
+          // this.cuenta_estado.set(cuenta.estado || null);
+          // this.cuenta_version_app.set(cuenta.version_app || null);
+          // this.cuenta_modo_conexion.set(cuenta.modo_conexion || null);
+          // this.cuenta_fecha_calificacion.set(cuenta.fecha_calificacion || null);
+          // this.cuenta_mensaje_calificacion.set(cuenta.mensaje_calificacion || null);
+          // this.cuenta_calificacion.set(cuenta.calificacion || null);
+          // this.cuenta_plan_mecodex.set(cuenta.plan_mecodex || null);
+          // this.cuenta_idapp_sta.set(cuenta.idapp_sta || null);
+          // this.cuenta_idapp_din.set(cuenta.idapp_din || null);
         }
       },
       error(err: any) {
