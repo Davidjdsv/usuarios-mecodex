f2cdbb08c4ca5206d72c0fe54fd2cef0ad8b36a6
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-12-18 09:27:51 -0500

    refactor(cuenta.service.ts): -Refactoriazación del código optimizando el servicio. Pasó de parametrizar todo a solo extraer del objeto -Se insertaron comentarios como docs en los métodos -Se cambió el constructor por un inject para angular moderno

--- src/app/core/services/cuenta.service.ts
+++ src/app/core/services/cuenta.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CuentaInterface, CuentaResponseInterface } from 'src/app/models/cuenta-interface';
 import { Observable, catchError, map, throwError } from 'rxjs';
@@ -13,47 +13,24 @@ export class CuentaService {
   // Signal para almacenar la licencia seleccionada
   idLicenciaSeleccionada = signal<number | null>(null);
 
-  constructor(private http: HttpClient) { }
+  private http = inject(HttpClient)
 
-  getCuenta(): Observable<CuentaInterface[]>{
-    const url = new URL(this.ApiCuentas());
-    return this.http.get<CuentaResponseInterface>(url.toString()).pipe(
-      map(res => {
-        return res.data.map((res: CuentaInterface) => ({
-          id: res.id,
-          correo: res.correo,
-          estado: res.estado,
-          gmail: res.gmail,
-          idapp_sta: res.idapp_sta,
-          idapp_din: res.idapp_din,
-          password: res.password,
-          fecha: res.fecha,
-          ultimo_salvado_datos: res.ultimo_salvado_datos,
-          calificacion: res.calificacion,
-          mensaje_calificacion: res.mensaje_calificacion,
-          fecha_calificacion: res.fecha_calificacion,
-          version_app: res.version_app,
-          modo_conexion: res.modo_conexion,
-          fecha_actualizacion_modo_conexion: res.fecha_actualizacion_modo_conexion,
-          estado_confirmacion_dispositivo: res.estado_confirmacion_dispositivo,
-          fecha_confirmacion_dispositivo: res.fecha_confirmacion_dispositivo,
-          estado_actualizacion_app: res.estado_actualizacion_app,
-          fecha_actualizacion_app: res.fecha_actualizacion_app,
-          estado_actualizacion_dominios: res.estado_actualizacion_dominios,
-          fecha_actualizacion_dominios: res.fecha_actualizacion_dominios,
-          sincronizacion_total: res.sincronizacion_total,
-          id_cliente: res.id_cliente,
-          id_pais: res.id_pais,
-          id_licencia: res.id_licencia,
-          plan_mecodex: res.plan_mecodex,
-          id_usuario_sensei: res.id_usuario_sensei,
-          fecha_creacion_cuenta: res.fecha_creacion_cuenta,
-          fecha_modificacion: res.fecha_modificacion,
-        }))
-      })
-    )
+  /**
+  * Obtiene todas las cuentas disponibles.
+  *
+  * @returns Observable con un array de objetos CuentaInterface.
+  */
+  getCuenta(): Observable<CuentaInterface[]> {
+    return this.http.get<CuentaResponseInterface>(this.ApiCuentas())
+    .pipe(map((res) => res.data));
   }
 
+  /**
+  * Crea una nueva cuenta para un cliente.
+  *
+  * @param cuenta Objeto con los datos de la cuenta a crear.
+  * @returns Observable con la cuenta creada.
+  */	
   createCuenta(cuenta: CuentaInterface): Observable<CuentaInterface>{
     const url = new URL(this.ApiCuentas());
     return this.http.post<CuentaInterface>(url.toString(), cuenta).pipe(
@@ -63,6 +40,13 @@ export class CuentaService {
     )
   }
 
+/**
+ * Actualiza la licencia asociada a un cliente.
+ *
+ * @param id_licencia Identificador de la licencia a asignar.
+ * @param id_cliente Identificador del cliente.
+ * @returns Observable con la cuenta actualizada.
+ */
   updateCuentaLicencia(id_licencia: number, id_cliente: any): Observable<CuentaInterface>{
     const url = new URL(this.ApiCuentas());
     url.searchParams.append('id_cliente', id_cliente.toString());
