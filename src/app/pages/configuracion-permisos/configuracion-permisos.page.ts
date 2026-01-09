4acad7db3c66597e04334a06d7df44e23a8cffe4
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2026-01-09 11:46:11 -0500

    feat(RBCA): -Creación de la plantilla para la creación de perfiles, roles y asignación de perfiles a alos usuarios del sistema

--- src/app/pages/configuracion-permisos/configuracion-permisos.page.ts
+++ src/app/pages/configuracion-permisos/configuracion-permisos.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonSegmentView,
+  IonSegmentContent,
+  IonCard,
+  IonCardContent,
+  IonIcon,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-configuracion-permisos',
   templateUrl: './configuracion-permisos.page.html',
   styleUrls: ['./configuracion-permisos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel,
+    IonSegmentView,
+    IonSegmentContent,
+    IonCard,
+    IonCardContent,
+    IonIcon,
+  ],
 })
 export class ConfiguracionPermisosPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
