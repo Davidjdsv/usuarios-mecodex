46e17d11a185910c90efee9c5a01f890252f1242
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2026-01-13 09:06:30 -0500

    --amend

--- src/app/core/services/permisos.service.ts
+++ src/app/core/services/permisos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { PermisosDataInterface, PermisosInterface, PermisosResponseInterface } from 'src/app/models/permisos';
+import { PermisosDataInterface, PermisosResponseInterface } from 'src/app/models/permisos';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
